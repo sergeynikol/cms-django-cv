442ce9842c4b15435b6b796a4b88b00ecabad52e
sergeynikol <104057963+sergeynikol@users.noreply.github.com>
2022-12-05 00:10:55 +0400

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -179,11 +179,12 @@ if not DEBUG:
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'ru'
 
 LANGUAGES = [
 
-    ('ru'),
+    ('ru', 'Russian'),
+    ('en', 'English')
 ]
 
 TIME_ZONE = 'UTC'
